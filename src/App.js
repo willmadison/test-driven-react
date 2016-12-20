368bc66050aaefc84fbb8c7bb5409de57a684915
Will <wmadisonDev@GMail.com>
2016-12-20 06:15:00 -0500

    Adding some additional tests to ensure full understanding of enzyme APIs and use cases with React.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+const App = props => {
+  const greeting = (props.emphasize) ? <em>{props.greeting}</em> : props.greeting;
+
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>{greeting}</h2>
       </div>
-    );
-  }
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+    </div>
+  );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import { render } from 'enzyme';
+import { render, shallow, mount } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
   // Static Rendering using enzyme.
   expect(render(<App />).text()).toContain('save to reload');
+});
+
+describe('Customized greetings', () => {
+  it('displays custom greetings', () => {
+    // Shallow rendering using enzyme.
+    const greeting = <h2>What's Up Patna?!</h2>;
+    expect(shallow(<App greeting="What's Up Patna?!" />).contains(greeting)).toBe(true);
+  });
+
+  it('displays custom greetings with emphasis', () => {
+    // Shallow rendering using enzyme.
+    const greeting = <h2><em>What's Up Patna?!</em></h2>;
+    expect(shallow(<App greeting="What's Up Patna?!" emphasize="true" />).contains(greeting)).toBe(true);
+  });
+
 });
